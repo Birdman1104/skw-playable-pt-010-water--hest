a76bac8ebf1408e05f5815b92a4a9d4e0f7687cb
Birdman1104 <arsenmazmanyan11@gmail.com>
2025-01-25 15:39:15 +0400

    URL update.

--- src/index.js
+++ src/index.js
@@ -22,8 +22,8 @@ function isAppleDevice() {
 
 window.CTACallImitation = () => {
     if (isAppleDevice()) {
-        window.open('https://apps.apple.com/ru/app/global-city-build-a-megapolis/id1455860184?l=en-GB');
+        window.open('https://apps.apple.com/us/app/pirate-treasures-gems-puzzle/id990972073');
     } else {
-        window.open('https://play.google.com/store/apps/details?id=com.rbx.city.android&hl=en');
+        window.open('https://play.google.com/store/apps/details?id=com.orangeapps.piratetreasure');
     }
 };
